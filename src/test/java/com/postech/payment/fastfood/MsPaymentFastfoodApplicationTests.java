2d10fd5385b96edca0bf44ccfeeb594cebbf2ff5
fernandeeess <matheusfernandesrodrigues@outlook.com>
2025-12-30 14:02:55 -0300

    test: add Mockito beans for SnsClient and MercadoPagoClient in integration tests

--- src/test/java/com/postech/payment/fastfood/MsPaymentFastfoodApplicationTests.java
+++ src/test/java/com/postech/payment/fastfood/MsPaymentFastfoodApplicationTests.java
@@ -1,14 +1,23 @@
 package com.postech.payment.fastfood;
 
+import com.postech.payment.fastfood.infrastructure.http.mercadopago.MercadoPagoClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 @ActiveProfiles("test")
 @SpringBootTest(properties = {
         "spring.flyway.enabled=false"
 })
 class MsPaymentFastfoodApplicationTests {
+
+    @MockitoBean
+    private SnsClient snsClient;
+
+    @MockitoBean
+    private MercadoPagoClient mercadoPagoClient;
 
     @Test
     void contextLoads() {
