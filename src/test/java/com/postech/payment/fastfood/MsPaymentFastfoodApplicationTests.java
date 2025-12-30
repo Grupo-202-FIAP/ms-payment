2302d9d7221dc63a44469e0442428339dd707ebe
fernandeeess <matheusfernandesrodrigues@outlook.com>
2025-12-30 14:39:40 -0300

    test: update Spring Boot test properties for AWS configuration

--- src/test/java/com/postech/payment/fastfood/MsPaymentFastfoodApplicationTests.java
+++ src/test/java/com/postech/payment/fastfood/MsPaymentFastfoodApplicationTests.java
@@ -8,8 +8,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @ActiveProfiles("test")
-@SpringBootTest(properties = {
-        "spring.profiles.active=${SPRING_PROFILES_ACTIVE:test}",
+@SpringBootTest(properties = {"spring.profiles.active=${SPRING_PROFILES_ACTIVE:test}",
         "server.port=${SERVER_PORT:8084}",
         "mercadoPago.publicKey=${PUBLIC_KEY:none}",
         "mercadoPago.accessToken=${ACCESS_TOKEN:none}",
@@ -17,14 +16,17 @@ import software.amazon.awssdk.services.sns.SnsClient;
         "mercadoPago.clientSecret=${CLIENT_SECRET:none}",
         "mercadoPago.externalPosID=${EXTERNAL_POS_ID:none}",
         "mercadoPago.webhook.secretKey=${WEBHOOK_SECRET:none}",
-        "cloud.aws.region.static=${AWS_REGION:us-east-1}",
-        "cloud.aws.credentials.access-key=${AWS_ACCESS_KEY:test}",
-        "cloud.aws.credentials.secret-key=${AWS_SECRET_KEY:test}",
+
+        // Propriedades do Spring Cloud AWS (v3.x+)
+        "spring.cloud.aws.region.static=${AWS_REGION:us-east-1}",
+        "spring.cloud.aws.credentials.access-key=${AWS_ACCESS_KEY:test}",
+        "spring.cloud.aws.credentials.secret-key=${AWS_SECRET_KEY:test}",
+        "spring.cloud.aws.sqs.endpoint=${SPRING_CLOUD_AWS_SQS_ENDPOINT:http://localhost:4566}",
+
         "spring.datasource.url=${SPRING_DATASOURCE_URL:jdbc:h2:mem:testdb}",
         "spring.datasource.username=${SPRING_DATASOURCE_USERNAME:sa}",
         "spring.datasource.password=${SPRING_DATASOURCE_PASSWORD:}",
         "cloud.aws.sqs.queues.process-payment-queue=${PAYMENT_QUEUE_NAME:test-queue}"
-        "spring.cloud.aws.sqs.endpoint=${SPRING_CLOUD_AWS_SQS_ENDPOINT:http://localhost:4566}"
 })
 class MsPaymentFastfoodApplicationTests {
     @MockitoBean
