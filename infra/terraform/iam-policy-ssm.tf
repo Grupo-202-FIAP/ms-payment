dd8ba7ddce20c13d28f2796fb741d20730e3444a
leobr1t0 <leonardo70177@gmail.com>
2026-01-09 01:01:54 -0300

    feat: ifnra k8s

--- infra/terraform/iam-policy-ssm.tf
+++ infra/terraform/iam-policy-ssm.tf
@@ -9,7 +9,7 @@ resource "aws_iam_policy" "ms_payment_ssm" {
         "ssm:GetParameter",
         "ssm:GetParameters"
       ]
-      Resource = "arn:aws:ssm:${data.aws_region.current.name}:<ACCOUNT_ID>:parameter/ms-payment/*" //ToDo: ajustar
+      Resource = "arn:aws:ssm:${var.aws_region}:056075870573:parameter/*"
     }]
   })
 }
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -9,38 +9,3 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
-
-variable "oidc_provider_arn" {
-  description = "ARN do OIDC provider do EKS"
-  type        = string
-}
-
-variable "oidc_provider_url" {
-  description = "URL do OIDC provider do EKS"
-  type        = string
-}
-
-variable "sqs_order_queue_name" {
-  description = "Nome da fila SQS de order"
-  type        = string
-}
-
-variable "sqs_order_callback_queue_name" {
-  description = "Nome da fila SQS de callback"
-  type        = string
-}
-
-variable "account_id" {
-  description = "ID da conta AWS"
-  type        = string
-}
-
-variable "eks_cluster_name" {
-  description = "Nome do cluster EKS"
-  type        = string
-}
-
-variable "terraform_state_bucket_name" {
-  description = "Nome do bucket S3 para armazenar o state do Terraform"
-  type        = string
-}
